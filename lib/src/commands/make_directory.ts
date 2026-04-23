d2b0906b96a4116aab3833347bc459d19e48ac41
Corno Schraverus <corno@schraverus.com>
2026-04-23 09:18:18 +0200

    pdt: published version 0.2.42

--- lib/src/commands/make_directory.ts
+++ lib/src/commands/make_directory.ts
@@ -9,6 +9,7 @@ import { __command_promise } from 'pareto-core/dist/__internals/async/command_pr
 
 //dependencies
 import { mkdir as fs_mkdir } from "fs"
+import { rm as fs_remove } from "fs"
 import * as t_path_to_text from "pareto-resources/dist/implementation/manual/transformers/path/text"
 
 export const $$: resources.commands.make_directory = __command((
@@ -16,27 +17,56 @@ export const $$: resources.commands.make_directory = __command((
 ) => {
     return __command_promise({
         'execute': (on_success, on_error) => {
-            fs_mkdir(
-                t_path_to_text.Node_Path($p),
-                {
-                    'recursive': true,
-                },
-                (err, path) => {
-                    if (err) {
-                        on_error({
-                            'path': $p,
-                            'type': _p.state.block(() => {
-                                if (err.code === 'EEXIST') {
-                                    return ['directory already exists', null]
-                                }
-                                throw new Error(`unhandled fs.mkdir error code: ${err.code}`)
+            const make_directory = () => {
+                fs_mkdir(
+                    t_path_to_text.Node_Path($p.path),
+                    {
+                        'recursive': true,
+                    },
+                    (err, path) => {
+                        if (err) {
+                            on_error({
+                                'path': $p.path,
+                                'type': _p.state.block(() => {
+                                    if (err.code === 'EEXIST') {
+                                        return ['directory already exists', null]
+                                    }
+                                    throw new Error(`unhandled fs.mkdir error code: ${err.code}`)
+                                })
                             })
-                        })
-                    } else {
-                        on_success()
+                        } else {
+                            on_success()
+                        }
                     }
-                }
-            )
+                )
+            }
+            if ($p['delete existing']) {
+                fs_remove(
+                    t_path_to_text.Node_Path($p.path),
+                    {
+                        'recursive': true,
+                        'force': true,
+                    },
+                    (err) => {
+                        if (err) {
+                            on_error({
+                                'path': $p.path,
+                                'type': _p.state.block(() => {
+                                    if (err.code === 'EACCES' || err.code === 'EPERM') {
+                                        return ['permission denied', null]
+                                    }
+                                    throw new Error(`unhandled fs.rm error code: ${err.code}`)
+                                })
+                            })
+                        } else {
+                            make_directory()
+                        }
+                    }
+                )
+
+            } else {
+                make_directory()
+            }
         }
     })
 })
