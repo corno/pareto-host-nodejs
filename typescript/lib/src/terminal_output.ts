be3fe8ad6e192698c4346c7ebad3a7afa7601f98
Corno Schraverus <corno@schraverus.com>
2026-06-18 07:58:24 +0200

    pdt: ...

--- typescript/lib/src/terminal_output.ts
+++ typescript/lib/src/terminal_output.ts
@@ -2,9 +2,6 @@ import * as p_a from 'pareto-core/dist/assign'
 
 import * as d_terminal_output from "pareto-resources/dist/interface/generated/liana/schemas/terminal_output/data"
 
-//shorthands
-import * as sh from "pareto-fountain-pen/dist/shorthands/prose"
-
 export const Message = ($: string): d_terminal_output.Message => {
     return {
         'raw': $,
