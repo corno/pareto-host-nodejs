c28e735394a244c7b1e10dfc094d68b4494b04f1
Corno Schraverus <corno@schraverus.com>
2026-07-07 14:43:35 +0200

    pdt: ...

--- typescript/lib/src/stream/commands/write_to_stderr.ts
+++
@@ -1,9 +0,0 @@
-import * as p_ from 'pareto-core/implementation/resource'
-
-//interface
-import * as interface_ from "pareto-stream/interface/commands"
-
-export const $$: interface_.commands.write_to_stderr = p_.command(($p, on_success) => {
-    process.stderr.write($p.data)
-    on_success()
-})
--- typescript/lib/src/stream/commands/write_to_stdout.ts
+++
@@ -1,9 +0,0 @@
-import * as p_ from 'pareto-core/implementation/resource'
-
-//interface
-import * as resources from "pareto-stream/interface/commands"
-
-export const $$: resources.commands.write_to_stdout = p_.command(($p, on_success) => {
-    process.stdout.write($p.data)
-    on_success()
-})
